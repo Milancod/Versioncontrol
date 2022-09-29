3b3f5ba26804795699f78b906cc372e016abebf3
milan <milanbtoth5@gmail.com>
2022-09-29 12:07:46 +0200

    Tetszoleges

--- UserMaintance/UserMaintance/Entities/User.cs
+++ UserMaintance/UserMaintance/Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UserMaintance.Entities
 {
-    internal class User
+  class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
         public string FirstName { get; set; }
