3bd9580069fd1fa89f027e48c9ffe1bdc7cfed9c
milan <milanbtoth5@gmail.com>
2022-10-02 20:11:10 +0200

    hh

--- UserMaintance/UserMaintance/Form1.cs
+++ UserMaintance/UserMaintance/Form1.cs
@@ -17,9 +17,10 @@ namespace UserMaintance
         public Form1()
         {
             InitializeComponent();
-            textBox1.Text = Resource1.LastName; // label1
-            textBox2.Text = Resource1.FirstName; // label2
-            button1.Text = Resource1.Add; // button1
+            textBox1.Text = Resource1.LastName; 
+            textBox2.Text = Resource1.FirstName; 
+            button1.Text = Resource1.Add; 
+            button2.Text = Resource1.Delete;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -34,11 +35,21 @@ namespace UserMaintance
         {
             var fel = new User()
             {
+                
                 LastName = textBox1.Text,
                 FirstName = textBox2.Text
 
             };
             users.Add(fel);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            
         }
     }
 }
