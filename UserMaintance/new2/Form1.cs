7984d75cb214070ceb8f95c2b7597348ede1591b
milan <milanbtoth5@gmail.com>
2022-10-21 14:06:04 +0200

    alma

--- UserMaintance/new2/Form1.cs
+++ UserMaintance/new2/Form1.cs
@@ -12,9 +12,33 @@ namespace new2
 {
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void fv()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            
+            var response = mnbService.GetExchangeRates(request);
+
+            
+            var result = response.GetExchangeRatesResult;
+        }
+        
     }
 }
