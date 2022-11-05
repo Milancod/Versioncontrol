600aed5aea9d73ee310fcff4b8b57cb0eaafc55a
milan <milanbtoth5@gmail.com>
2022-11-05 17:28:27 +0100

    commit

--- UserMaintance/new3/Entities/Ball.cs
+++ UserMaintance/new3/Entities/Ball.cs
@@ -11,26 +11,11 @@ namespace new3.Entities
 {
     public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = 50;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            //throw new NotImplementedException();
-            DrawImage(e.Graphics);
-        }
-        protected void DrawImage(Graphics g)
+       
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
-       public void MoveBall()
-        {
-            Left += 1;
-        }
+       
     }
 }
