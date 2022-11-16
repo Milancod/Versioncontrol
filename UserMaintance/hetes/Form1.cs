0b51a0c912da9e73752eb20864e26f40a222002f
milan <milanbtoth5@gmail.com>
2022-11-16 15:19:31 +0100

    commit

--- UserMaintance/hetes/Form1.cs
+++ UserMaintance/hetes/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using hetes.Entities;
 
 namespace hetes
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource=Ticks;
+            CreatePortfolio();
+
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
     }
 }
