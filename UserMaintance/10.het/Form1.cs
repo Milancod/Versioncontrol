65c3c1bb03f6b5ffbae64172193a319bbd756218
milan <milanbtoth5@gmail.com>
2022-12-08 22:28:05 +0100

    commit

--- UserMaintance/10.het/Form1.cs
+++ UserMaintance/10.het/Form1.cs
@@ -15,11 +15,37 @@ namespace _10.het
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            gc.AddPlayer();
+            gc.Start(true);
+            gc.GameOver += Gc_GameOver1;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+        }
+
+        private void Gc_GameOver1(object sender)
+        {
+           
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
 
         private void Form1_Load(object sender, EventArgs e)
